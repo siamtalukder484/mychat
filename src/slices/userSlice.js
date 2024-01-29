7bcb4e9aeeb9facd2949944dddf53066b716c84b
Siam Talukder <73644547+siamtalukder484@users.noreply.github.com>
2024-01-29 19:11:01 +0600

    Update userSlice.js

--- src/slices/userSlice.js
+++ src/slices/userSlice.js
@@ -9,10 +9,10 @@ export const counterSlice = createSlice({
   initialState,
   reducers: {
     increment: (state) => {
-      state.value += 1
+      state.value = 1
     },
     decrement: (state) => {
-      state.value -= 1
+      state.value = 1
     },
     incrementByAmount: (state, action) => {
       state.value += action.payload
