de1ae6ff56a09fd4958d3398c0acf5e44805eb6d
Siam Talukder <73644547+siamtalukder484@users.noreply.github.com>
2024-02-07 19:44:33 +0600

    userlist show, friend request send and show and cencel

--- src/pages/home/FriendRequest.jsx
+++ src/pages/home/FriendRequest.jsx
@@ -1,26 +1,69 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import GroupCard from '../../components/home/GroupCard'
 import Image from '../../utilities/Image'
+import { useSelector, useDispatch } from 'react-redux'
+import { getDatabase, ref, onValue, set,push,remove } from "firebase/database";
+import { ToastContainer, toast } from 'react-toastify';
 
 const FriendRequest = () => {
+
+  const db = getDatabase();
+  const data = useSelector((state) => state.loginuserdata.value)
+  const [fRequest, setfRequest] = useState()
+
+
+  useEffect(()=>{
+    const fRequestRef = ref(db, 'friendrequest');
+    onValue(fRequestRef, (snapshot) => {
+      let arr = []
+      snapshot.forEach((item)=>{
+        console.log(item.val());
+        if(data.uid == item.val().receiverid){
+          arr.push({...item.val(),id:item.key})
+        }
+      })
+      setfRequest(arr)
+    });
+  },[])
+  
+ let handleCancelFRequest = (cancelinfo) => {
+    console.log(cancelinfo);
+    remove(ref(db, "friendrequest/" + cancelinfo.id)).then(()=>{
+      toast("Request Cancel..")
+    })
+    
+ }
+
   return (
     <>
+        <ToastContainer/>
          <GroupCard cardtitle="Friend Request">
           <div className='usermainbox'>
-            <div className='useritem'>
-                <div className='userimgbox'>
-                  <Image source="" alt="img"/>
-                </div>
-                <div className='userinfobox'>
-                  <div>
-                      <h3>anik</h3>
-                      <p>MERN Developer</p>
+            {fRequest && fRequest.length > 0 ?
+            fRequest.map((item,index)=>(
+              <div key={index} className='useritem'>
+                  <div className='userimgbox'>
+                    <Image source={item.senderimg} alt="img"/>
                   </div>
-                  <button className='addbutton'>
-                    accept
-                  </button>
-                </div>
-            </div>
+                  <div className='userinfobox'>
+                    <div>
+                        <h3>{item.sendername}</h3>
+                        <p>MERN Developer</p>
+                    </div>
+                    <div className='buttongroup'>
+                      <button className='addbutton'>
+                        accept
+                      </button>
+                      <button onClick={()=>handleCancelFRequest(item)} className='addbutton'>
+                        cancel
+                      </button>
+                    </div>
+                  </div>
+              </div>
+            ))
+            :
+            <h2>No Request Found ....</h2>
+            }
           </div>
       </GroupCard>
     </>
--- src/pages/home/UserList.jsx
+++ src/pages/home/UserList.jsx
@@ -1,97 +1,72 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import GroupCard from '../../components/home/GroupCard'
 import { TiPlus } from 'react-icons/ti'
 import Image from '../../utilities/Image'
+import { getDatabase, ref, onValue, set,push } from "firebase/database";
+import { useSelector, useDispatch } from 'react-redux'
+import { ToastContainer, toast } from 'react-toastify';
 
 const UserList = () => {
+  const [userList, setUserList] = useState()
+  const db = getDatabase();
+  const data = useSelector((state) => state.loginuserdata.value)
+  
+
+  useEffect(()=>{
+    const userRef = ref(db, 'users');
+    onValue(userRef, (snapshot) => {
+      let arr = []
+      snapshot.forEach((item)=>{
+        if(data.uid != item.key){
+          arr.push({...item.val(),id:item.key})
+        }
+      })
+      setUserList(arr)
+    });
+  },[])
+
+  let handleFRequest = (frequestinfo) => {
+    set(push(ref(db, "friendrequest")),{
+      senderid: data.uid,
+      sendername: data.displayName,
+      senderimg: data.photoURL,
+      senderemail: data.email,
+      receiverid: frequestinfo.id,
+      receivername: frequestinfo.username,
+      receiveremail: frequestinfo.email,
+      receiverimg: frequestinfo.profileimg
+    }).then(()=>{
+      toast("Friend Request Send Successfully..")
+    })
+  }
+
+
   return (
     <>
+        <ToastContainer/>
         <GroupCard cardtitle="User List">
           <div className='usermainbox'>
-            <div className='useritem'>
-                <div className='userimgbox'>
-                  <Image source="" alt="img"/>
-                </div>
-                <div className='userinfobox'>
-                  <div>
-                      <h3>anik</h3>
-                      <p>MERN Developer</p>
+            {userList && userList.length > 0 
+            ?
+            userList.map((item,index)=>(
+              <div key={index} className='useritem'>
+                  <div className='userimgbox'>
+                    <Image source={item.profileimg} alt="img"/>
                   </div>
-                  <button className='addbutton'>
-                    <TiPlus />
-                  </button>
-                </div>
-            </div>
-            <div className='useritem'>
-                <div className='userimgbox'>
-                  <Image source="" alt="img"/>
-                </div>
-                <div className='userinfobox'>
-                  <div>
-                      <h3>anik</h3>
-                      <p>MERN Developer</p>
+                  <div className='userinfobox'>
+                    <div>
+                        <h3>{item.username}</h3>
+                        <p>MERN Developer</p>
+                    </div>
+                    <button onClick={()=>handleFRequest(item)} className='addbutton'>
+                      <TiPlus />
+                    </button>
                   </div>
-                  <button className='addbutton'>
-                    <TiPlus />
-                  </button>
-                </div>
-            </div>
-            <div className='useritem'>
-                <div className='userimgbox'>
-                  <Image source="" alt="img"/>
-                </div>
-                <div className='userinfobox'>
-                  <div>
-                      <h3>anik</h3>
-                      <p>MERN Developer</p>
-                  </div>
-                  <button className='addbutton'>
-                    <TiPlus />
-                  </button>
-                </div>
-            </div>
-            <div className='useritem'>
-                <div className='userimgbox'>
-                  <Image source="" alt="img"/>
-                </div>
-                <div className='userinfobox'>
-                  <div>
-                      <h3>anik</h3>
-                      <p>MERN Developer</p>
-                  </div>
-                  <button className='addbutton'>
-                    <TiPlus />
-                  </button>
-                </div>
-            </div>
-            <div className='useritem'>
-                <div className='userimgbox'>
-                  <Image source="" alt="img"/>
-                </div>
-                <div className='userinfobox'>
-                  <div>
-                      <h3>anik</h3>
-                      <p>MERN Developer</p>
-                  </div>
-                  <button className='addbutton'>
-                    <TiPlus />
-                  </button>
-                </div>
-            </div>
-            <div className='useritem'>
-                <div className='userimgbox'>
-                  <Image source="" alt="img"/>
-                </div>
-                <div className='userinfobox'>
-                  <div>
-                      <h3>anik</h3>
-                      <p>MERN Developer</p>
-                  </div>
-                  <button className='addbutton'>
-                    <TiPlus />
-                  </button>
-                </div>
-            </div>
+              </div>
+            ))
+            :
+            <h2>No user available</h2>
+            }
           </div>
       </GroupCard>
     </>
